8b0eb484b3711dffe75ceda85cca854206d4f79a
Tibor Mag <39742102+bitiplz@users.noreply.github.com>
2022-04-24 10:47:12 +0200

    docs, jest coverage, readme

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
-{
-  reporters: ['default', 'jest-junit']
+module.exports = {
+  reporters: ['default', 'jest-junit'],
+  coverageReporters: ['json-summary'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'features/**/*.js',
+    'lib/**/*.js',
+    'hooks/**/*.js',
+    'components/**/*.jsx',
+    'layouts/**/*.jsx',
+    'pages/**/*.jsx',
+  ],
 }
