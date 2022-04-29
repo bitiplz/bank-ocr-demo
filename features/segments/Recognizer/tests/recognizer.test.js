c16f26f4396e84c640744106bd345c2b1f77595a
Tibor Mag <39742102+bitiplz@users.noreply.github.com>
2022-04-29 19:41:56 +0200

    patch up tests somewhat

--- features/segments/Recognizer/tests/recognizer.test.js
+++ features/segments/Recognizer/tests/recognizer.test.js
@@ -1,126 +1,141 @@
 import recognizeRecord from '../index'
+import segments from 'features/segments'
 import { CHARACTER_MAP } from 'features/segments/Parser/defaults'
 
 describe('recognizer', () => {
-  test('valid number input stays unchanged with status ok', async () => {
-    const input = '939288737'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    const config = { autoCorrectRule: 'perInput' }
+  const inputFrom = (word, autoCorrectRule = 'perInput') => [
+    word.split(''),
+    word.split('').map((char) => CHARACTER_MAP[char]),
+    CHARACTER_MAP,
+    { autoCorrectRule },
+  ]
 
-    const { value, status } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
+  test('Valid input stays unchanged, status OK', async () => {
+    const args = inputFrom('939288737')
 
-    const valueMatches = value.join('') === input
-    const statusOk = status === 'OK'
+    const { value, status } = recognizeRecord(...args)
 
-    expect(valueMatches && statusOk).toBeTruthy()
+    expect(value.join('')).toBe('939288737')
+    expect(status).toBe('OK')
   })
 
-  test('unrecognizable number input stays unchanged with status err', async () => {
-    const input = '93928873?'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    inputArray[inputArray.length - 1] = '_________'
-    const config = { autoCorrectRule: 'perInput' }
+  test('Unrecognizable input stays unchanged, status ERR', async () => {
+    const args = inputFrom('?39288737')
+    args[1][0] = '_________'
 
-    const { value, status } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
+    const { value, status } = recognizeRecord(...args)
 
-    const valueMatches = value.join('') === input
-    const statusOk = status === 'ERR'
-
-    expect(valueMatches && statusOk).toBeTruthy()
+    expect(value.join('')).toBe('?39288737')
+    expect(status).toBe('ERR')
   })
 
-  test('perchar, recognizable invalid changes with status amb', async () => {
-    const input = '13928873?'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    inputArray[inputArray.length - 1] = ' _   |   '
-    const config = { autoCorrectRule: 'perChar' }
+  test('PerChar. Recognizable invalid input changes, status AMB', async () => {
+    const args = inputFrom('13928873?', 'perChar')
+    args[1].splice(-1, 1, ' _   |   ')
 
-    const { value, status } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
+    const { value, status } = recognizeRecord(...args)
 
-    const valueMatches = value.join('') === '139288737'
-    const statusOk = status === 'AMB'
-
-    expect(valueMatches && statusOk).toBeTruthy()
+    expect(value.join('')).toBe('139288737')
+    expect(status).toBe('AMB')
   })
 
-  test('recognizable changes and turns to ok', async () => {
-    const input = '93928873?'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    inputArray[inputArray.length - 1] = ' _   |   '
-    const config = { autoCorrectRule: 'perInput' }
+  test('PerChar. Multiple not trivials, status ERR', async () => {
+    const args = inputFrom('?????????')
+    args[1].map(() => '     |   ')
 
-    const { value, status } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
+    const { value, status } = recognizeRecord(...args)
 
-    const valueMatches = value.join('') === '939288737'
-    const statusOk = status === 'OK'
-
-    expect(valueMatches && statusOk).toBeTruthy()
+    expect(value.join('')).toBe('?????????')
+    expect(status).toBe('ERR')
   })
 
-  test('perInput rule with multiple trivial ?s, all changes', async () => {
-    const input = '???288737'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    inputArray[0] = ' _   |   '
-    inputArray[1] = ' _   |   '
-    inputArray[2] = ' _   |   '
-    const config = { autoCorrectRule: 'perInput' }
+  test('Recognizable invalid input changes, status ILL', async () => {
+    const args = inputFrom('93928873?')
+    args[1].splice(-1, 1, '     |   ')
 
-    const { value } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
-    const valueMatches = value.join('') === '777288737'
+    const { value, status } = recognizeRecord(...args)
 
-    expect(valueMatches).toBeTruthy()
+    expect(value.join('')).toBe('939288731')
+    expect(status).toBe('ILL')
   })
 
-  test('multiple ? no changes, stays err', async () => {
-    const input = '???288737'
-    const charArr = input.split('')
-    const inputArray = charArr.map((char) => CHARACTER_MAP[char])
-    const five = ' _ |_  _|'
-    inputArray[0] = five
-    inputArray[1] = five
-    inputArray[2] = five
-    const config = { autoCorrectRule: 'perInput' }
+  test('PerInput. Multiple trivial ?s, all changes', async () => {
+    const args = inputFrom('???288737')
+    args[1].splice(0, 3, ...Array(3).fill(' _   |   '))
 
-    const { value, status } = recognizeRecord(
-      charArr,
-      inputArray,
-      CHARACTER_MAP,
-      config
-    )
+    const { value } = recognizeRecord(...args)
 
-    const valueMatches = value.join('') === input
-    const statusOk = status === 'ERR'
+    expect(value.join('')).toBe('777288737')
+  })
 
-    expect(valueMatches && statusOk).toBeTruthy()
+  test('Rule none, stays unchanged. status ERR', async () => {
+    const args = inputFrom('?39288737', 'none')
+    args[1][0] = '         '
+
+    const { value, status } = recognizeRecord(...args)
+
+    expect(value.join('')).toBe('?39288737')
+    expect(status).toBe('ERR')
+  })
+
+  test('Multiple ?s, no changes status ERR', async () => {
+    const args = inputFrom('???288737')
+    args[1].splice(-1, 1, ' _ |_  _|')
+
+    const { value, status } = recognizeRecord(...args)
+
+    expect(value.join('')).toBe('???288737')
+    expect(status).toBe('ERR')
+  })
+
+  test('From segments e2e', async () => {
+    // prettier-ignore
+    const input =   Array(9).fill(
+                      ' _  _  _  _  _  _  _  _  _ \n' +
+                      ' _| _| _| _| _| _| _| _| _|\n' +
+                      '|_ |_ |_ |_ |_ |_ |_ |_ |_ \n'
+                    ).join("\n")
+
+    const result = segments(input)
+
+    const { data, messages } = result
+    const {
+      postProcessData: { value, status },
+    } = data[0]
+
+    expect(data.length).toBe(9)
+    expect(messages.length).toBe(0)
+
+    expect(value.join('')).toBe('222222222')
+    expect(status).toBe('ILL')
+  })
+
+  test('From segments e2e, multiple', async () => {
+    // prettier-ignore
+    const input =  
+        '    _  _     _  _  _  _  _ \n' +
+        '  | _| _||_||_ |_   ||_||_|\n' +
+        '  ||_  _|  | _||_|  ||_| _|\n\n' +
+        '    _  _     _  _  _  _  _ \n' +
+        '  | _| _||_||_ |_   ||_||_|\n' +
+        '                           \n\n' +
+        ' _  _  _  _  _  _  _  _  _ \n' +
+        ' _| _C _| _| _| _| _| _| _|\n' +
+        '|_ |_ |_ |_ |_ Q_ |_ xxx|_ \n'
+
+    const result = segments(input)
+
+    const { data, messages } = result
+    const results = data.map(({ postProcessData: { value, status } }) => ({
+      value,
+      status,
+    }))
+
+    expect(results.length).toBe(3)
+    expect(messages.length).toBe(0)
+
+    expect(results[0].value.join('')).toBe('123456789')
+    expect(results[1].value.join('')).toBe('1??4??7??')
+    expect(results[2].value.join('')).toBe('2?222?2?2')
   })
 })
