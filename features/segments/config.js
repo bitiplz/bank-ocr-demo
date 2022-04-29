49ba720a2ae7b96d4b46d6de19b528d4ef88c9bc
Tibor Mag <39742102+bitiplz@users.noreply.github.com>
2022-04-29 20:25:08 +0200

    add hex chars

--- features/segments/config.js
+++ features/segments/config.js
@@ -2,7 +2,40 @@ const segmentsConfig = {
   processor: {
     autoCorrectRule: 'perInput',
   },
-  parser: {},
+  parser: {
+    extendMap: true,
+    // prettier-ignore
+    characterMap:
+    {
+        A:   ' _ '
+            +'|_|'
+            +'| |'
+        ,
+        B:   ' _ '
+            +'|_\\'
+            +'|_/'
+
+        ,
+        C:   ' _ '
+            +'|  '
+            +'|_ '
+
+        ,
+        D:   ' _ '
+            +'| \\'
+            +'|_/'
+
+        ,
+        E:   ' _ '
+            +'|_ '
+            +'|_ '
+
+        ,
+        F:   ' _ '
+            +'|_ '
+            +'|  '
+    },
+  },
 }
 
 export default segmentsConfig
